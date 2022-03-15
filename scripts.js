460f3f500b8c3620f4466c5de68a89005033c18f
ruancs <ruan.cs@hotmail.com>
2022-03-15 20:04:18 -0300

    funções botão

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
 function clicou(){
 
-    var a = 'asidjaosidjaiosdjaiosjd'
-    document.querySelector('h1').innerText = a.length;
+    var a = ['Lorem ipsum dolor sit amet',
+    'consectetur adipiscing elit',
+    'Nullam tempus tincidunt elementum',
+    ' Vivamus rhoncus sagittis ipsum, non hendrerit '];
+
+
+     document.querySelector('h1').innerText = a[Math.floor((Math.random() * a.length))];
+     
+
 
     
     // var a = document.querySelector("button");
