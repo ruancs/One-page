273045e1b6b867aa1c57b37d826066e3632adc80
ruancs <ruan.cs@hotmail.com>
2022-03-15 19:46:17 -0300

    criando funçoes em botão

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function clicou(){
+
+    var a = 'asidjaosidjaiosdjaiosjd'
+    document.querySelector('h1').innerText = a.length;
+
+    
+    // var a = document.querySelector("button");
+    
+}
