1abb0534d881ef19c48385e5f101ada3fee2a6d7
ruancs <ruan.cs@hotmail.com>
2022-03-16 19:53:35 -0300

    adicionado cards e footer

--- scripts.js
+++ scripts.js
@@ -3,10 +3,11 @@ function clicou(){
     var a = ['Lorem ipsum dolor sit amet',
     'consectetur adipiscing elit',
     'Nullam tempus tincidunt elementum',
-    ' Vivamus rhoncus sagittis ipsum, non hendrerit '];
+    'Vivamus rhoncus sagittis ipsum, non hendrerit', ];
 
 
      document.querySelector('h1').innerText = a[Math.floor((Math.random() * a.length))];
+
      
 
 
